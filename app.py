a79c949b5da32f347f421f0669bc6c846b08b222
rlondon3 <codesmithr@outlook.com>
2023-09-10 22:05:46 -0700

    add sql to create tables for: users, product, orders, and products_orderd.

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, request
 
+CREATE_USERS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username VARCHAR(100), password VARCHAR(100));"
+)
+CREATE_PRODUCTS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS product (id SERIAL PRIMARY KEY, name VARCHAR(100), description  VARCHAR(250), price NUMBER(5, 2));"
+)
+CREATE_ORDERS_TABLE = (
+    "CREATE TABLE orders (id SERIAL PRIMARY KEY, order_status VARCHAR(100), user_id bigint REFERENCES users(id) ON DELETE CASCADE);"
+)
+CREATE_PRODUCTS_ORDERED_TABLE = (
+    "CREATE TABLE products_ordered (id SERIAL PRIMARY KEY, order_id bigint REFERENCES orders(id) ON DELETE CASCADE, products_id bigint REFERNCES products(id) ON DELETE RESTRICT, quantity integer);"
+)
+
 app = Flask(__name__)
 
 @app.route("/")
